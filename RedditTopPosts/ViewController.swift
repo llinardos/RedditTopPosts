99d1ab5e4f0ce7beed7fbede4895b2a5b19cc7c4
Leandro Linardos <leandro.linardos@gmail.com>
2020-02-03 07:57:11 -0300

    Initial Commit

---
+++ RedditTopPosts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditTopPosts
+//
+//  Created by Leandro Linardos on 03/02/2020.
+//  Copyright © 2020 Leandro Linardos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
