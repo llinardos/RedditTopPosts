a8590ec2074b5c24ee87f1bed5c224d124a3d589
Leandro Linardos <leandro.linardos@gmail.com>
2020-02-03 09:02:33 -0300

    Add empty view on Detail screen, it suggest the user to select a post on the list (because on iPad we show the detail without a post selected).

--- RedditTopPosts/PostDetailVC.swift
+++ RedditTopPosts/PostDetailVC.swift
@@ -1,11 +1,26 @@
 import UIKit
 
 class PostDetailVC: UIViewController {
+  @IBOutlet private weak var contentView: UIView!
+  @IBOutlet private weak var emptyView: UIView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     title = "Selected Post"
     
     self.view.backgroundColor = .white
+    
+    showEmpty()
+  }
+  
+  private func showEmpty() {
+    self.emptyView.isHidden = false
+    self.contentView.isHidden = true
+  }
+  
+  private func showPost(_ post: RedditPost) {
+    self.emptyView.isHidden = true
+    self.contentView.isHidden = false
   }
 }
