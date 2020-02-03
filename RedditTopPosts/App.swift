1728002ca169ca46fe49eff52b39ecb7ca9d67a8
Leandro Linardos <leandro.linardos@gmail.com>
2020-02-03 11:57:00 -0300

    Fix layout with safe area.

--- RedditTopPosts/App.swift
+++ RedditTopPosts/App.swift
@@ -63,7 +63,7 @@ extension App: UISplitViewControllerDelegate {
 func dummyPosts() -> [RedditPost] {
   return [
     RedditPost(
-      title: "There is a finite number of people who were born the same year as you. Every time one of them dies, you are closer to being the last one.",
+      title: (1...10).map { _ in "There is a finite number of people who were born the same year as you. Every time one of them dies, you are closer to being the last one." }.joined() ,
       authorName: "Author 1",
       commentsCount: 0, creationDate:
       Date(),
