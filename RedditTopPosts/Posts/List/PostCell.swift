6afe5bd91922d517a319c2bb648dbc471140ffaa
Leandro Linardos <leandro.linardos@gmail.com>
2020-02-03 16:20:58 -0300

    Move cell content setup outside from cell.

--- RedditTopPosts/Posts/List/PostCell.swift
+++ RedditTopPosts/Posts/List/PostCell.swift
@@ -1,12 +1,20 @@
 import UIKit
 
 class PostCell: UITableViewCell {
+  struct Content {
+    var authorText: String
+    var titleText: String
+    var commentsText: String
+    var dateText: String
+    var imageURL: URL?
+  }
+  
   @IBOutlet private weak var authorLabel: UILabel!
   @IBOutlet private weak var titleLabel: UILabel!
   @IBOutlet private weak var commentsCountLabel: UILabel!
   @IBOutlet private weak var dateLabel: UILabel!
   @IBOutlet private weak var thumbnailView: RedditPostRemoteImageView!
-  @IBOutlet private weak var readUnreadBadge: UIView!
+  @IBOutlet private weak var badgeView: UIView!
   @IBOutlet private weak var dismissPostButton: UIButton!
   
   var onDismiss: () -> Void = {}
@@ -37,44 +45,18 @@ class PostCell: UITableViewCell {
     accessoryView?.tintColor = .white
   }
   
-  func showPost(_ post: RedditPost, asRead isRead: Bool) {
-    authorLabel.text = post.authorName
-    titleLabel.text = post.title
-    commentsCountLabel.text = text(for: post.commentsCount)
-    dateLabel.text = text(for: post.creationDate)
-    thumbnailView.loadImageFromURL(post.image?.thumbnailURL)
-    readUnreadBadge.isHidden = isRead
+  func setContent(_ content: Content) {
+    authorLabel.text = content.authorText
+    titleLabel.text = content.titleText
+    commentsCountLabel.text = content.commentsText
+    dateLabel.text = content.dateText
+    thumbnailView.loadImageFromURL(content.imageURL)
   }
   
-  func showAsRead() {
-    readUnreadBadge.isHidden = true
+  func showBadge(_ show: Bool) {
+    badgeView.isHidden = !show
   }
-  
-  private func text(for commentsCount: Int) -> String {
-    // TODO: This can be handled better with localization + strings dict: https://developer.apple.com/library/archive/documentation/MacOSX/Conceptual/BPInternational/StringsdictFileFormat/StringsdictFileFormat.html
-    switch commentsCount {
-    case 0: return "No comments yet"
-    case 1: return "1 comment"
-    default: return "\(commentsCount) comments"
-    }
-  }
-  
-  private func text(for creationDate: Date) -> String {
-    let timeEllapsedSinceCreation = Date().timeIntervalSince(creationDate).rounded(.up) // TODO: here we have an implicit parameter, should be extracted to make it testable
-    let formatter = DateComponentsFormatter()
-    formatter.calendar = Calendar.current // TODO: remember that we have a "hidden" singleton here, should be extracted to make it testeable
-    formatter.allowedUnits = [.year, .month, .day, .hour, .minute, .second]
-    formatter.unitsStyle = .full
-    formatter.maximumUnitCount = 1
     
-    guard let prettyTimeEllapsedString = formatter.string(from: timeEllapsedSinceCreation) else {
-      // TODO: not sure when formatter.string() can fail, but a force unwrap will produce a crash in that case.
-      // I prefer to return an "empty string" and log the error on a crash/error report sysmtem
-      return "--"
-    }
-    return prettyTimeEllapsedString + " ago"
-  }
-  
   @IBAction private func onDismissButtonTap(_ sender: Any) {
     onDismiss()
   }
