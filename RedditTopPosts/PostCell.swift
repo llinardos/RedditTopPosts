8d746abb7b3823a146d06f3e44573f5b88354def
Leandro Linardos <leandro.linardos@gmail.com>
2020-02-03 08:57:41 -0300

    Show "pretty" creation date with the format "seconds/minutes/... ago".

--- RedditTopPosts/PostCell.swift
+++ RedditTopPosts/PostCell.swift
@@ -34,15 +34,31 @@ class PostCell: UITableViewCell {
     authorLabel.text = post.authorName
     titleLabel.text = post.title
     commentsCountLabel.text = text(for: post.commentsCount)
-    dateLabel.text = "\(post.creationDate.timeIntervalSince1970)"
+    dateLabel.text = text(for: post.creationDate)
   }
   
   private func text(for commentsCount: Int) -> String {
-    // TODO: LEAN - This can be handled better with localization + strings dict: https://developer.apple.com/library/archive/documentation/MacOSX/Conceptual/BPInternational/StringsdictFileFormat/StringsdictFileFormat.html
+    // TODO: This can be handled better with localization + strings dict: https://developer.apple.com/library/archive/documentation/MacOSX/Conceptual/BPInternational/StringsdictFileFormat/StringsdictFileFormat.html
     switch commentsCount {
     case 0: return "No comments yet"
     case 1: return "1 comment"
     default: return "\(commentsCount) comments"
     }
+  }
+  
+  private func text(for creationDate: Date) -> String {
+    let timeEllapsedSinceCreation = Date().timeIntervalSince(creationDate).rounded(.up) // TODO: here we have an implicit parameter, should be extracted to make it testable
+    let formatter = DateComponentsFormatter()
+    formatter.calendar = Calendar.current // TODO: remember that we have a "hidden" singleton here, should be extracted to make it testeable
+    formatter.allowedUnits = [.year, .month, .day, .hour, .minute, .second]
+    formatter.unitsStyle = .full
+    formatter.maximumUnitCount = 1
+    
+    guard let prettyTimeEllapsedString = formatter.string(from: timeEllapsedSinceCreation) else {
+      // TODO: not sure when formatter.string() can fail, but a force unwrap will produce a crash in that case.
+      // I prefer to return an "empty string" and log the error on a crash/error report sysmtem
+      return "--"
+    }
+    return prettyTimeEllapsedString + " ago"
   }
 }
